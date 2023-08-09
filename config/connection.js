f080754912c2d59221f5b5af13b94a77e4f5e91a
guymorganb <guymorganb@gmail.com>
2023-08-08 20:49:56 -0500

    changed connections.js logic

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const connectDB = () => {
-  const username = encodeURIComponent(process.env.MONGOADMIN);
-  const password = encodeURIComponent(process.env.MONGOPASS);
-  const dbName = 'Test'; // Connects to capital 'T'est database
 
-  const mongoURI = `mongodb://${username}:${password}@localhost:27017/${dbName}?authSource=${dbName}`;
-
-  mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error(err));
+const connectDB = async () => {
+  try {
+    const username = encodeURIComponent(process.env.MONGOADMIN);
+    const password = encodeURIComponent(process.env.MONGOPASS);
+    const dbName = 'Test'; // Connects to capital 'T'est database
     // The options {useNewUrlParser: true, useUnifiedTopology: true} are provided to avoid deprecation warnings.
+    const mongoURI = `mongodb://${username}:${password}@localhost:27017/${dbName}?authSource=${dbName}`;
+
+    await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log(`Connected to MongoDB, DB:${dbName}`);
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
 };
 
 module.exports = connectDB;
